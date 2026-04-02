0dd2f97fb82438f880f0424beb7ec6f74b620567
atulraghuwanshi84-collab <atulraghuwanshi84@gmail.com>
2026-04-02 15:50:17 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : import.meta.env.MODE=== "development" ? "https://chatifycomplete.onrender.com" : "/api",
-    withCredentials : true
-})
+  baseURL: import.meta.env.MODE === "development" 
+    ? "http://localhost:5000"        // local backend for development
+    : "https://chatifycomplete.onrender.com",  // render backend for production
+  withCredentials: true,
+});
